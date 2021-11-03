c14754ada485fe7be3d57947b01775b8f9fe84d8
redrosein <redroseinhouse@gmail.com>
2021-11-03 19:11:32 +0900

    MFC Initialize

---
+++ FrameWork/MAPTOOL/Include/MAPTOOLView.cpp
@@ -0,0 +1,105 @@
+﻿
+// MAPTOOLView.cpp: CMAPTOOLView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "MAPTOOL.h"
+#endif
+
+#include "MAPTOOLDoc.h"
+#include "MAPTOOLView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMAPTOOLView
+
+IMPLEMENT_DYNCREATE(CMAPTOOLView, CView)
+
+BEGIN_MESSAGE_MAP(CMAPTOOLView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CMAPTOOLView 생성/소멸
+
+CMAPTOOLView::CMAPTOOLView() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CMAPTOOLView::~CMAPTOOLView()
+{
+}
+
+BOOL CMAPTOOLView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CMAPTOOLView 그리기
+
+void CMAPTOOLView::OnDraw(CDC* /*pDC*/)
+{
+	CMAPTOOLDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+
+// CMAPTOOLView 인쇄
+
+BOOL CMAPTOOLView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CMAPTOOLView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CMAPTOOLView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+
+// CMAPTOOLView 진단
+
+#ifdef _DEBUG
+void CMAPTOOLView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CMAPTOOLView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CMAPTOOLDoc* CMAPTOOLView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMAPTOOLDoc)));
+	return (CMAPTOOLDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMAPTOOLView 메시지 처리기
